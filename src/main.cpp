2da999b64bf64bf136c0b9c3cbf6f697d59d1d26
Victor <vvfernandesneto@gmail.com>
2026-07-15 20:07:32 -0300

    showing sprite on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include "bn_core.h"
+#include "bn_sprite_ptr.h"
+#include "bn_sprite_items_player.h"
+
 
 int main()
 {
     bn::core::init();
+
+    bn::sprite_ptr player = bn::sprite_items::player.create_sprite(0, 0);
 
     while(true)
     {
