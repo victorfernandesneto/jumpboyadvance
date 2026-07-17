c7294994e1a1f12d9cce90b9300e4d6b5294b126
Victor <vvfernandesneto@gmail.com>
2026-07-16 21:06:35 -0300

    added horizontal movement, refactored player to player_sprite

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,27 @@
 #include "bn_core.h"
+#include "bn_keypad.h"
 #include "bn_sprite_ptr.h"
-#include "bn_sprite_items_player.h"
+#include "bn_sprite_items_player_sprite.h"
 
 
 int main()
 {
     bn::core::init();
 
-    bn::sprite_ptr player = bn::sprite_items::player.create_sprite(0, 0);
+    bn::sprite_ptr player_sprite = bn::sprite_items::player_sprite.create_sprite(0, 0);
 
     while(true)
     {
+        /*
+        The centerline of the player sprite is 8 to the left and 4 to the left,
+        since the right side has 4px of transparency in it.
+        */
+        if(bn::keypad::left_held() && player_sprite.x() > -112){
+            player_sprite.set_x(player_sprite.x() - 1);
+        }
+        else if(bn::keypad::right_held() && player_sprite.x() < 116){
+            player_sprite.set_x(player_sprite.x() + 1);
+        }
         bn::core::update();
     }
 }
