2c8da85996214735a35e3efce5aa8a8d3bfbef92
Victor <vvfernandesneto@gmail.com>
2026-07-17 05:46:46 -0300

    simple gravity added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "bn_core.h"
 #include "bn_keypad.h"
 #include "bn_sprite_ptr.h"
+#include "bn_fixed.h"
+
+
 #include "bn_sprite_items_player_sprite.h"
 
 
@@ -10,18 +13,39 @@ int main()
 
     bn::sprite_ptr player_sprite = bn::sprite_items::player_sprite.create_sprite(0, 0);
 
+    bn::fixed gravity = 0.1;
+    bn::fixed velocityY = 0;
+    bn::fixed ground = 72;
+    int velocityX = 1;
+
     while(true)
     {
+        if(player_sprite.y() < ground){
+            if(velocityY < 2){
+            velocityY+=gravity;
+            }
+        }
+        else{
+            velocityY = 0;
+        }
+
+
+
         /*
         The centerline of the player sprite is 8 to the left and 4 to the left,
         since the right side has 4px of transparency in it.
         */
         if(bn::keypad::left_held() && player_sprite.x() > -112){
-            player_sprite.set_x(player_sprite.x() - 1);
+            player_sprite.set_x(player_sprite.x() - velocityX);
         }
         else if(bn::keypad::right_held() && player_sprite.x() < 116){
-            player_sprite.set_x(player_sprite.x() + 1);
+            player_sprite.set_x(player_sprite.x() + velocityX);
         }
+        if(bn::keypad::a_pressed()){
+            velocityY = -2;
+        }
+        
+        player_sprite.set_y(player_sprite.y() + velocityY);
         bn::core::update();
     }
 }
